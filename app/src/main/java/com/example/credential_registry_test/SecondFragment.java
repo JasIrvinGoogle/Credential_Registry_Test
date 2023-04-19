3e6370fc25cc50f38c0ae03d6edcacec84e0a0fb
physicalrobot <kimboho@gmail.com>
2023-04-19 09:02:32 -0700

    updated UI added delete feature to attributes

--- app/src/main/java/com/example/credential_registry_test/SecondFragment.java
+++ app/src/main/java/com/example/credential_registry_test/SecondFragment.java
@@ -1,5 +1,7 @@
 package com.example.credential_registry_test;
 
+import static android.widget.AdapterView.*;
+
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -22,7 +24,7 @@ import androidx.navigation.fragment.NavHostFragment;
 import com.example.credential_registry_test.databinding.FragmentSecondBinding;
 
 public class SecondFragment extends Fragment {
-    String[] item = {"mDoc", "something1", "something2"};
+    String[] item = {"mDoc", "password", "passkey"};
     AutoCompleteTextView autoCompleteTextView;
 
     ArrayAdapter<String> adapterItems;
@@ -59,17 +61,43 @@ public class SecondFragment extends Fragment {
         // on below line we are initializing our variables.
         languageLV = view.findViewById(R.id.idLVLanguages);
         addBtn = view.findViewById(R.id.idBtnAdd);
-        itemEdt = view.findViewById(R.id.idEdtItemName);
+        itemEdt = view.findViewById(R.id.listAttributes);
         lngList = new ArrayList<>();
         // on below line we are adding items to our list
-        lngList.add("C++");
-        lngList.add("Python");
+        // lngList.add("C++");
+        // lngList.add("Python");
 
         // on the below line we are initializing the adapter for our list view.
         ArrayAdapter<String> adapter = new ArrayAdapter<>(getActivity(), android.R.layout.simple_list_item_1, lngList);
 
         // on below line we are setting adapter for our list view.
         languageLV.setAdapter(adapter);
+
+//        languageLV.setOnItemClickListener(new OnItemClickListener() {
+//            @Override
+//            public void onItemClick(AdapterView<?> parent, View view,int i, int position, long id) {
+////                Toast.makeText("Removed: " + lngList.get(i));
+//                removeItem();
+//                return false;
+//            }
+//        });
+
+
+
+        languageLV.setOnItemClickListener(new OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> a, View v, int position, long id) {
+
+                Toast.makeText(getActivity(), "Removed: " +lngList.get(position), Toast.LENGTH_SHORT).show();
+                lngList.remove(position);
+                adapter.notifyDataSetChanged();
+                adapter.notifyDataSetInvalidated();
+            }
+
+        });
+
+
+
 
         // on below line we are adding click listener for our button.
         addBtn.setOnClickListener(new View.OnClickListener() {
@@ -83,6 +111,7 @@ public class SecondFragment extends Fragment {
 
                     // on below line we are adding item to our list.
                     lngList.add(item);
+                    itemEdt.getText().clear();
 
                     // on below line we are notifying adapter
                     // that data in list is updated to
@@ -101,12 +130,11 @@ public class SecondFragment extends Fragment {
 
 
 
-
         autoCompleteTextView = getView().findViewById(R.id.auto_complete_txt);
 
         adapterItems = new ArrayAdapter<String>(getActivity() , R.layout.list_item,item);
         autoCompleteTextView.setAdapter(adapterItems);
-        autoCompleteTextView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        autoCompleteTextView.setOnItemClickListener(new OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
                 String item = adapterView.getItemAtPosition(i).toString();
@@ -129,6 +157,18 @@ public class SecondFragment extends Fragment {
     }
 
 
+    public void removeItem(int remove){
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(getActivity(), android.R.layout.simple_list_item_1, lngList);
+
+        lngList.remove(remove);
+
+        // on below line we are notifying adapter
+        // that data in list is updated to
+        // update our list view.
+        adapter.notifyDataSetChanged();
+
+
+    }
 
     @Override
     public void onDestroyView() {
